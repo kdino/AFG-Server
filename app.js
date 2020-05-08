208bef187322f58d39bcbc03c744eb81304da0bf
이기동/연구원/ID SST개발Project(ki.lee@lge.com) <ki.lee@lge.com>
2020-05-08 17:15:27 +0900

    AWS S3에 파일 업로드 모듈 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const AWS = require("aws-sdk");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+const s3bucket = AWS.S3({params: {Bucket: 'afgbucket'}});
 
 const mongodbID = kidong;
 const mongodbPW = kizzong1;
