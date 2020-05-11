b74b5bad4ff35dfa9df506b8a6efba25ab86430a
이기동/연구원/ID SST개발Project(ki.lee@lge.com) <ki.lee@lge.com>
2020-05-11 11:34:47 +0900

    post로 image 업로드하는 라우트 추가

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
 //jshint esversion:6
 
+const s3API = require("./aws_S3");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
-const AWS = require("aws-sdk");
+const mongoose = require("mongoose");
+const multer = require('multer'); // express에 multer모듈 적용 (for 파일업로드)
+const upload = multer({ dest: 'uploads/' })
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
-const s3bucket = AWS.S3({params: {Bucket: 'afgbucket'}});
+const s3bucket = AWS.S3({ params: { Bucket: "afgbucket" } });
 
 const mongodbID = kidong;
 const mongodbPW = kizzong1;
 const DBnameToconnect = test;
 
-mongoose.connect("mongodb+srv://" + mongodbID + ":" + mongodbPW + "@afg-db-qsh3u.mongodb.net/" + DBnameToconnect +"?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}); // allows to use local MongoDB 
+mongoose.connect(
+  "mongodb+srv://" +
+    mongodbID +
+    ":" +
+    mongodbPW +
+    "@afg-db-qsh3u.mongodb.net/" +
+    DBnameToconnect +
+    "?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+); // allows to use local MongoDB
 
 const articleSchema = new mongoose.Schema({
   title: String,
-  content: String
+  content: String,
 });
 
 const Article = mongoose.model("Article", articleSchema);
 
-
 //////////////////// Request targeting All Articles ////////////////
-app.route("/articles")
-  .get(function(req, res) {
-    Article.find(function(err, foundArticles) {
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
       if (err) {
         res.send(err);
       } else {
@@ -45,16 +59,16 @@ app.route("/articles")
     });
   })
 
-  .post(function(req, res) {
+  .post(function (req, res) {
     console.log(req.body.title);
     console.log(req.body.content);
 
     const newArticle = new Article({
       title: req.body.title,
-      content: req.body.content
+      content: req.body.content,
     });
 
-    newArticle.save(function(err) {
+    newArticle.save(function (err) {
       if (err) {
         res.send(err);
       } else {
@@ -63,8 +77,8 @@ app.route("/articles")
     });
   })
 
-  .delete(function(req, res) {
-    Article.deleteMany({}, function(err) {
+  .delete(function (req, res) {
+    Article.deleteMany({}, function (err) {
       if (err) {
         res.send(err);
       } else {
@@ -74,29 +88,36 @@ app.route("/articles")
   });
 
 //////////////////// Request targeting specific Articles ////////////////
-app.route("/articles/:articleTitle")
-  .get(function(req, res) {
-    Article.find({
-      title: req.params.articleTitle
-    }, function(err, foundArticle) {
-      if (err) {
-        res.send("No articles matched!");
-      } else {
-        res.send(foundArticle);
+app
+  .route("/articles/:articleTitle")
+  .get(function (req, res) {
+    Article.find(
+      {
+        title: req.params.articleTitle,
+      },
+      function (err, foundArticle) {
+        if (err) {
+          res.send("No articles matched!");
+        } else {
+          res.send(foundArticle);
+        }
       }
-    });
+    );
   })
 
-  .put(function(req, res) {
-    Article.update({
-        title: req.params.articleTitle
-      }, {
+  .put(function (req, res) {
+    Article.update(
+      {
+        title: req.params.articleTitle,
+      },
+      {
         title: req.body.title,
-        content: req.body.content
-      }, {
-        overwrite: true
+        content: req.body.content,
+      },
+      {
+        overwrite: true,
       }, // mongoDB는 바꿀 parameter를 다 채우면 overwrite True가 되나 mongoose에서는 false로 남아있기에 수동으로 바꿔줌.
-      function(err) {
+      function (err) {
         if (err) {
           res.send("No articles matched!");
         } else {
@@ -106,33 +127,44 @@ app.route("/articles/:articleTitle")
     );
   })
 
-  .patch(function(req, res) {
-    Article.update({
-      title: req.params.articleTitle
-    }, {
-      $set: req.body
-    }, function(err) {
-      if (err) {
-        res.send("No articles matched!");
-      } else {
-        res.send("Successfully updated data!");
+  .patch(function (req, res) {
+    Article.update(
+      {
+        title: req.params.articleTitle,
+      },
+      {
+        $set: req.body,
+      },
+      function (err) {
+        if (err) {
+          res.send("No articles matched!");
+        } else {
+          res.send("Successfully updated data!");
+        }
       }
-    });
+    );
   })
 
-  .delete(function(req, res) {
-    Article.deleteOne({
-      title: req.params.articleTitle
-    }, function(err) {
-      if (err) {
-        res.send("No articles matched!");
-      } else {
-        res.send("Successfully deleted data!");
+  .delete(function (req, res) {
+    Article.deleteOne(
+      {
+        title: req.params.articleTitle,
+      },
+      function (err) {
+        if (err) {
+          res.send("No articles matched!");
+        } else {
+          res.send("Successfully deleted data!");
+        }
       }
-    })
+    );
   });
 
+app.route("/api/pictures")
+  .post(upload.single('myfile'), function(req, res){
+    //s3API.upload(req.file.);
+  });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
--- aws_S3.js
+++ aws_S3.js
@@ -1,31 +1,39 @@
-var AWS = require("aws-sdk");
-AWS.config.region = 'ap-northeast-2';
+var s3API = {};
 
-const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const AWS = require("aws-sdk");
+AWS.config.region = "ap-northeast-2";
+
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 // const param = {
 //     'Bucket': 'afgbucket',
 //     'Key' : 'myImage',
 //     'ACL' : 'public-read'
 // }
 
-var uploadParams = {Bucket: process.argv[2], Key: '', Body: ''};
+var uploadParams = { Bucket: process.argv[2], Key: "", Body: "" };
 var file = process.argv[3];
 
 // Configure the file stream and obtain the upload parameters
-var fs = require('fs');
+const fs = require("fs");
 var fileStream = fs.createReadStream(file);
-fileStream.on('error', function(err) {
-  console.log('File Error', err);
+fileStream.on("error", function (err) {
+  console.log("File Error", err);
 });
 uploadParams.Body = fileStream;
-var path = require('path');
+const path = require("path");
 uploadParams.Key = path.basename(file);
 
 // call S3 to retrieve upload file to specified bucket
-s3.upload (uploadParams, function (err, data) {
-  if (err) {
-    console.log("Error", err);
-  } if (data) {
-    console.log("Upload Success", data.Location);
-  }
-});
+s3API.upload = function () {
+  
+  s3.upload(uploadParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    }
+    if (data) {
+      console.log("Upload Success", data.Location);
+    }
+  });
+};
+
+module.exports = s3API;
