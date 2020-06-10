05c3ecb83a9c757e06e268caa080d06480e79c76
이기동/연구원/ID SST개발Project(ki.lee@lge.com) <ki.lee@lge.com>
2020-06-10 09:20:49 +0900

    bibles API - 여러 구절도 리턴되도록 수정

--- routes/bibles.js
+++ routes/bibles.js
@@ -6,22 +6,49 @@ const EngBible = require("../models/engbible");
 // https://stackoverflow.com/questions/40967927/multiple-get-parameter-with-express?noredirect=1&lq=1
 
 router.get("/kor", function (req, res) {
-  KorBible.findVerse(req.query.book, req.query.chapter, req.query.verse)
-    .then((words) => {
-      if (!words) return res.status(400).send({ "result": "verse not found" });
-      res.status(200).send(words);
+  KorBible.findVerse(
+    req.query.book,
+    req.query.chapter,
+    req.query.verse1,
+    req.query.verse2
+  )
+    .then((returnObj) => {
+      if (!returnObj)
+        return res.status(400).send({ result: "verse not found" });
+      var returnVerse = "";
+      Array.prototype.forEach.call(returnObj, (element) => {
+        returnVerse += element.words + " ";
+      });
+      res.status(200).send({
+        result: "success",
+        words: returnVerse.substring(0, returnVerse.length - 1),
+      });
     })
     .catch((err) => res.status(500).send(err));
 });
 
 router.get("/eng", function (req, res) {
-  EngBible.findVerse(req.query.book, req.query.chapter, req.query.verse)
-    .then((words) => {
-      if (!words) return res.status(400).send({ "result": "verse not found" });
-      res.status(200).send(words);
+  EngBible.findVerse(
+    req.query.book,
+    req.query.chapter,
+    req.query.verse1,
+    req.query.verse2
+  )
+    .then((returnObj) => {
+      if (!returnObj)
+        return res.status(400).send({ result: "verse not found" });
+      var returnVerse = "";
+      Array.prototype.forEach.call(returnObj, (element) => {
+        returnVerse += element.words + " ";
+      });
+      res
+        .status(200)
+        .send({
+          result: "success",
+          words: returnVerse.substring(0, returnVerse.length - 1),
+        });
     })
     .catch((err) => res.status(500).send(err));
 });
-
 
 module.exports = router;
