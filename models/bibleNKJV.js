d9349a09dafd8f4aaaa8d5b76c403b27abd5d58d
Ki <kdlee93@gmail.com>
2020-10-01 22:09:23 +0900

    역본 추가 개역개정, 새번역, NIV, NKJV

---
+++ models/bibleNKJV.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+// Schema Define
+
+const bibleSchema = new mongoose.Schema({
+  book: { type: Number, required: true },
+  chapter: { type: Number, required: true },
+  verse: { type: Number, required: true },
+  words: { type: String },
+});
+
+bibleSchema.statics.findVerse = function (book, chapter, verse1, verse2) {
+	var returnObj;
+	if (verse1 === verse2) {
+	  returnObj = this.find({ book, chapter, verse: verse1 });
+	} else {
+	  returnObj = this.find({
+		book,
+		chapter,
+		verse: { $gte: verse1, $lte: verse2 },
+	  }).sort({ key: 1 });
+	}
+  
+	return returnObj;
+  };
+
+// Create Model and Export
+module.exports = mongoose.model("BibleNKJV", bibleSchema, "bibleNIV");
--- routes/bibles.js
+++ routes/bibles.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
-const KorBible = require("../models/korbible");
-const EngBible = require("../models/engbible");
+const BibleNIV = require("../models/bibleNIV");
+const BibleNKJV = require("../models/bibleNKJV");
+const BibleRevision = require("../models/bibleRevision");
+const BibleStandard = require("../models/bibleStandard");
 
 // Find a word
 // https://stackoverflow.com/questions/40967927/multiple-get-parameter-with-express?noredirect=1&lq=1
 
-router.get("/kor", function (req, res) {
-  KorBible.findVerse(
+router.get("/niv", function (req, res) {
+  BibleNIV.findVerse(
     req.query.book,
     req.query.chapter,
     req.query.verse1,
@@ -27,8 +29,8 @@ router.get("/kor", function (req, res) {
     .catch((err) => res.status(500).send(err));
 });
 
-router.get("/eng", function (req, res) {
-  EngBible.findVerse(
+router.get("/nkjv", function (req, res) {
+  BibleNKJV.findVerse(
     req.query.book,
     req.query.chapter,
     req.query.verse1,
@@ -47,6 +49,50 @@ router.get("/eng", function (req, res) {
           result: "success",
           words: returnVerse.substring(0, returnVerse.length - 1),
         });
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+router.get("/revision", function (req, res) {
+  BibleRevision.findVerse(
+    req.query.book,
+    req.query.chapter,
+    req.query.verse1,
+    req.query.verse2
+  )
+    .then((returnObj) => {
+      if (!returnObj)
+        return res.status(400).send({ result: "verse not found" });
+      var returnVerse = "";
+      Array.prototype.forEach.call(returnObj, (element) => {
+        returnVerse += element.words + " ";
+      });
+      res.status(200).send({
+        result: "success",
+        words: returnVerse.substring(0, returnVerse.length - 1),
+      });
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+router.get("/standard", function (req, res) {
+  BibleStandard.findVerse(
+    req.query.book,
+    req.query.chapter,
+    req.query.verse1,
+    req.query.verse2
+  )
+    .then((returnObj) => {
+      if (!returnObj)
+        return res.status(400).send({ result: "verse not found" });
+      var returnVerse = "";
+      Array.prototype.forEach.call(returnObj, (element) => {
+        returnVerse += element.words + " ";
+      });
+      res.status(200).send({
+        result: "success",
+        words: returnVerse.substring(0, returnVerse.length - 1),
+      });
     })
     .catch((err) => res.status(500).send(err));
 });
